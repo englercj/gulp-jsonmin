1efc605520f61b040a6db1384718fb0d67582300
Romain Faust <romain.faust@gmail.com>
2018-08-19 15:48:52 +0200

    Update out-of-date dependencies

--- test/jsonmin.spec.js
+++ test/jsonmin.spec.js
@@ -1,27 +1,20 @@
-'use strict';
-
+const assert = require('assert');
+const File = require('vinyl');
 const tap = require('tap');
-const assert = require("assert");
-const File = require('gulp-util').File;
-const jsonmin = require('../index');
 
-tap.test('jsonmin() should minify json files', function (t) {
+const jsonmin = require('..');
 
-    const stream = jsonmin();
-    const json = '{\n    "hello": "world"\n}';
+tap.test('jsonmin() should minify json files', test => {
+    const json = '{\n "hello": "world" \n}';
     const expected = '{"hello":"world"}';
 
-    stream.on("finish", function () {
-        t.end();
-    });
-
-    stream.on("data", function (file) {
-        assert.equal(file.contents.toString(), expected);
-    });
-
-    stream.write(new File({
-        contents: new Buffer(json)
-    }));
+    const stream = jsonmin()
+        .on('data', file => {
+            assert.strictEqual(file.contents.toString(), expected);
+        })
+        .on('finish', () => {
+            test.end();
+        });
+    stream.write(new File({ contents: Buffer.from(json) }));
     stream.end();
-
 });
